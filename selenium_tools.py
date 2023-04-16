5ace3929b70bf4231f181e0c4fae40e2040e404e
Karlyn Galante <46430073+kgalante18@users.noreply.github.com>
2023-04-16 16:12:31 -0400

    Create selenium_tools.py
    adding some selenium tools for the screenshot tool

---
+++ selenium_tools.py
@@ -0,0 +1,52 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+class SeleniumWebInteraction:
+
+    def __init__(self, driver_path = None):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+
+        self.drive = webdriver.Chrome(r'C:/scraper_dependencies/chromedriver.exe')
+
+    def scraper_directory(self):
+        '''
+        Sets up the directory dependency to drop the save files into by default.
+        '''
+        import os
+
+        if not os.path.exists('./screenshot/'):
+            os.mkdir('./screenshot/')
+        else: 
+            print("screenshot directory exists")
+
+    def navigate_to_url(self, url):
+        '''
+        Instructs the chromedriver to load the webpage
+        '''
+
+        self.driver.get(url)
+
+    def launch_browser(self, options=None):
+        from selenium import webdriver
+        
+        self.driver = webdriver.Chrome(self.driver_path, options=options)
+
+    def click_XPATH_element(self, xpath:str):
+        element = self.driver.find_element(By.XPATH, xpath)
+        element.click()
+
+    def fill_form(self, xpath:str, value):
+        field = self.driver.find_element(By.XPATH, xpath)
+        field.clear()
+        field.send_keys(value)
+
+    def close(self):
+        self.driver.quit()
+
+
+
+
+
